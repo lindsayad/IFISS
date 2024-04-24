f6bebc9cbee3149d6899b4d40a95d5f253ec755f
Alex Lindsay <alexander.lindsay@inl.gov>
2024-04-24 09:00:35 -0700

    Checkin .m files

---
+++ ifiss3.7/helpme_octave.m
@@ -0,0 +1,15 @@
+%HELPME_OCTAVE  Octave specific issues 
+%   IFISS scriptfile: DJS; 8 August 2023.
+% Copyright (c) 2012 D.J. Silvester, H.C. Elman, A. Ramage
+fprintf(' \n');
+fprintf(' IFISS 3.7 \n')
+fprintf(' The Octave package is identical to the MATLAB package\n');
+fprintf(' \n');
+fprintf(' Here is a list of minor "issues" with testing with Octave 8.2 \n');
+fprintf(' *  There are a host of warning messages associated with \n')
+fprintf('    incompatibility (mostly due to sloppy syntax) \n');
+fprintf(' *  The generation of movies requires additional Octave toolbox\n');
+fprintf('    functionality (there is no "WriteVideo" in standard Octave)\n');
+fprintf(' \n');
+fprintf(' Please report any other compatibility problems that you encounter\n') 
+fprintf(' to the Octave Bug Report website.\n\n');
